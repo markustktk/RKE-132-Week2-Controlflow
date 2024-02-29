638069d09b3214272b893a0ea27c15222378892e
Markus Rei. <markus.reinumae@tktk.ee>
2024-02-29 16:26:11 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//rakendus palub kasutajal sisestada oma vanus
+//kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse  "You are too young to use IG".
+//else: konsoolis kuvatakse "Welcome to IG".
+
+Console.WriteLine("Enter your age:");
+
+string userAge = Console.ReadLine();
+
+int userAgeNum = 0;
+
+//boolean = true/false
+
+bool isAgeNumber = Int32.TryParse(userAge, out userAgeNum);
+Console.WriteLine($"Parse result {isAgeNumber}. User is {userAgeNum} years old.");
+
+if(isAgeNumber)
+{
+    if (userAgeNum >= 13) {
+        Console.WriteLine("Welcome to IG!");
+                         }
+    else { Console.WriteLine("You are too young to use IG!");
+         }
+}
+else
+    {
+        Console.WriteLine("Could not read your age");
+}
+//int userAge = Int32.Parse(Console.ReadLine()); //"13" - heap, 13 - stack
+
+
+/*
+if (userAge >= 13)
+{
+    Console.WriteLine("Welcome to IG!");
+} else //if (userAge < 13)
+{
+    Console.WriteLine("You are too young to use IG.");
+}
+
+*/
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Markus!");
+
+//rakendus küsib kasuatajalt tema sugu
+//rakendus laseb kasutajal sisestada perekonnanime
+//lähtudes kasutaja valikust kuvab rakendus tervituse
+//"Welcome, Mr. Perekonnanimi" / "Welcome Ms. Perekonnanimi"
+
+Console.WriteLine("Please select your Gender (M/F)");
+
+char userGender = char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter your last name:");
+
+string userLastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName}");
+} 
+else if (userGender== 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}");
+} else
+{
+    Console.WriteLine($"Welcome! {userLastName}");
+}
+
+//Console.WriteLine($"Welcome, {userGender}");
